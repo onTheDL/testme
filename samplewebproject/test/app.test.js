0193dc13c6f86dce726683aafa610bc19aecd3d2
Daniel Laserna <daniel.laserna@protonmail.com>
2021-04-18 22:53:13 -0400

    Add test for an invalid email. Refactor to use assert

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -20,5 +20,21 @@ it("shows a success message with a valid email", async () => {
 
   const h1 = dom.window.document.querySelector("h1");
 
-  console.log('Contents of h1', h1.innerHTML);
+  assert.strictEqual(h1.innerHTML, "Looks good!");
+});
+
+it("shows a fail message with an invalid email", async () => {
+  const dom = await render("index.html");
+  const input = dom.window.document.querySelector("input");
+
+  input.value = "fsewrbsdfa";
+
+  // simulate a submit event
+  dom.window.document
+    .querySelector("form")
+    .dispatchEvent(new dom.window.Event("submit"));
+
+  const h1 = dom.window.document.querySelector("h1");
+
+  assert.strictEqual(h1.innerHTML, "Invalid email");
 });
